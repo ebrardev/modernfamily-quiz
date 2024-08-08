411874967c08865f71ad992844ec8939bbdf6674
ebrardev <16155052@mersin.edu.tr>
2024-08-08 14:38:36 +0300

    Initial commit

---
+++ src/quizData.js
@@ -0,0 +1,55 @@
+const quizData = [
+    {
+      question: "Which character is known for his love of magic and often performs tricks?",
+      options: ["Phil Dunphy", "Mitchell Pritchett", "Cam Tucker", "Jay Pritchett"],
+      answer: "Phil Dunphy"
+    },
+    {
+      question: "What is the name of Gloria's son from her first marriage?",
+      options: ["Manny", "Joe", "Luke", "Alex"],
+      answer: "Manny"
+    },
+    {
+      question: "Which character has a closet business?",
+      options: ["Claire Dunphy", "Mitchell Pritchett", "Jay Pritchett", "Cam Tucker"],
+      answer: "Jay Pritchett"
+    },
+    {
+      question: "What is the name of Cam and Mitchell's adopted daughter?",
+      options: ["Lily", "Haley", "Alex", "Claire"],
+      answer: "Lily"
+    },
+    {
+      question: "Which character is known for their high-pitched voice and eccentric personality?",
+      options: ["Cam Tucker", "Mitchell Pritchett", "Phil Dunphy", "Gloria Pritchett"],
+      answer: "Gloria Pritchett"
+    },
+    {
+      question: "What is the name of the dog that Jay and Gloria own?",
+      options: ["Stella", "Buddy", "Max", "Duke"],
+      answer: "Stella"
+    },
+    {
+      question: "Which character often says 'I’m not a pervert. I’m just Italian.'?",
+      options: ["Phil Dunphy", "Cam Tucker", "Gloria Pritchett", "Jay Pritchett"],
+      answer: "Gloria Pritchett"
+    },
+    {
+      question: "What sport does Manny play in high school?",
+      options: ["Basketball", "Football", "Fencing", "Baseball"],
+      answer: "Fencing"
+    },
+    {
+      question: "Which character is known for their extreme competitiveness?",
+      options: ["Claire Dunphy", "Phil Dunphy", "Mitchell Pritchett", "Jay Pritchett"],
+      answer: "Claire Dunphy"
+    },
+    {
+      question: "What is the name of Mitchell and Claire's mother?",
+      options: ["Dede", "Gloria", "Lily", "Cam"],
+      answer: "Dede"
+    }
+  ];
+  
+  export default quizData;
+  
